9fdfb37d3671d12214445b4a52639b3fddafe842
mohtishim <mohtishim5@gmail.com>
2025-02-12 01:19:14 +0500

    initial

---
+++ app.js
@@ -0,0 +1,7 @@
+// alert("Mohtishim");
+// alert("Mohiuddin");
+// alert("mohtishim5@gmail.com");
+// alert(+923160238035);
+// alert(12345678);
+// alert("You're learning JavaScript!");
+alert("I like nothing")
