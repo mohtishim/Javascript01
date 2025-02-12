80b0a37b93d4fe80443d570f211e66152627a526
mohtishim <mohtishim5@gmail.com>
2025-02-12 17:37:02 +0500

    ok ki report

--- app.js
+++ app.js
@@ -1,7 +1,47 @@
+// Chapter 1
+
+
 // alert("Mohtishim");
 // alert("Mohiuddin");
 // alert("mohtishim5@gmail.com");
 // alert(+923160238035);
 // alert(12345678);
 // alert("You're learning JavaScript!");
-alert("I like nothing")
+// alert("I like nothing")
+
+// Chapter 2
+
+// var myWebsiteName = "Helllo World!";
+
+// var a;
+// a = "Project";
+
+// alert(a);
+
+// var teamName = "Alpha";
+// alert(teamName);
+
+// var bestMan = "Charlie";
+// bestMan = "John";
+// alert(bestMan);
+
+// Chapter 3
+
+// var caseQty;
+// caseQty = 144;
+
+// var num = Number("9");
+// alert(num);
+
+// var sum;
+// sum = 10 + 2;
+// alert(sum);
+
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// alert(orderTotal)
+
+var lastNumber = 50;
+lastNumber = lastNumber + 20;
+alert(lastNumber)
