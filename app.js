20ae6a866ee29554b8f78d47515a33eeec953483
mohtishim <mohtishim5@gmail.com>
2025-02-19 00:42:12 +0500

    done beruu

--- app.js
+++ app.js
@@ -40,8 +40,147 @@
 // var merchTotal = 100;
 // var shippingCharge = 10;
 // var orderTotal = merchTotal + shippingCharge;
-// alert(orderTotal)
+// alert(orderTotal);
 
-var lastNumber = 50;
-lastNumber = lastNumber + 20;
-alert(lastNumber)
+// var lastNumber = 50;
+// lastNumber = lastNumber + 20;
+// alert(lastNumber);
+
+
+
+// Assignment # 2
+// VARIABLES FOR STRINGS
+
+// Q1
+// var userName;
+
+// Q2
+// var myName = "Mohtishim Mohiuddin";
+
+// Q3
+// a)
+// var message;
+// b)
+// message = "Hello World";
+// c)
+// alert(message);
+
+// Q4
+// var name = "Jhone Doe";
+// var age = "15 years old";
+// var bio = "Certified Mobile App Developer";
+// alert(name);
+// alert(age);
+// alert(bio);
+
+// Q5
+// alert(" PIZZA \n PIZZ \n PIZ \n PI \n P");
+
+// Q6
+// var email = "mohtishimmoin@gmail.com";
+// var message = "This is my email address " + email;
+// alert(message)
+
+// Q7
+// var book = "A smarter way to learn JavaScript";
+// var message = "I am trying to learn from the Book " + book;
+// alert(message);
+
+// Q8
+// var content = "Yah! I can write HTML content through JavaScript";
+// document.write(content);
+
+// Q9
+// var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(design);
+// document.write(design);
+
+
+
+// VARIABLE NAMES: LEGAL & ILLEGAL
+// Assignment # 4
+
+// Q1
+// var myName = "Mohtishim", age = "29 years old", occupation = "Web Developer";
+// console.log("My Name is", myName, age, "and I am", occupation);
+// The above console log is just for checking
+
+// Q2
+// 5 Legal variable
+
+// var myName;
+// var my_name;
+// var MyName;
+// var $alpha;
+// var name;
+
+// 5 Illegal Variable
+
+// var .name;
+// var @name;
+// var ?name;
+// var 0123;
+// var 0name;
+
+// Q3
+// a
+// document.write("Rules for naming JS variables")
+
+// b
+// alpha_Bravo, Alpha_Bravo, $myName, _myName12
+
+// c 
+// letter, $ , _
+
+// d 
+// sensitive
+
+// e 
+// keyword
+
+
+// VARIABLES FOR NUMBERS
+// Assignment # 3
+
+// Q1 
+// var age = 29;
+// alert("I am "+age+" years old");
+
+// Q2 
+
+// var visit = 14;
+// alert("You have visited "+visit+" times a day");
+
+// Q3
+// var birthYear = 1995;
+// document.write("My birth year is", birthYear);
+
+// Q4
+// var visitorName = "John Doe";
+// var product = "T-shirt";
+// var quantity = "100";
+
+// document.write(visitorName, "ordered", quantity, product+ "(s) on XYZ Clothing Store");
+
+// Alerts
+// Assignment # 1
+
+// Q1
+// alert("welcome..");
+
+// Q2
+// document.write("Error! Please enter a valid password");
+
+// Q3
+// alert("Welcome to JS Land...\nHappy Coding")
+
+
+// Q4
+alert("Welcome to JS Land");
+alert("Happy coding!\nPrevent this page from creating additional dialogs");
+
+// Q5
+alert("Hello... I can run JS through my web browsers console");
+
+// Q6/7 done 
+
